994f955a093e7ebfb07592a7299ba0935c385b56
edac99 <jamjam1208@naver.com>
2024-02-26 17:16:31 +0900

    feat: Holder, Issuer, Verifier 서버에 swagger 설치

--- BE/issuer/src/main.ts
+++ BE/issuer/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: ['http://localhost:8081', 'http://holder:8081'],
   });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Service API Docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8082);
 }
 bootstrap();
