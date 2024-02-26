d2ec7473a9515099780ee37b3e67a9ffb86a42eb
edac99 <jamjam1208@naver.com>
2024-02-26 18:11:45 +0900

    feat: Issuer의 createUserVC API 틀 구현
    VC 생성 블록체인 코드 필요
    키 체인 적재 블록체인 코드 필요

--- BE/issuer/src/app.module.ts
+++ BE/issuer/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
+import { IssuerAPIModule } from './issuer/issuer-api.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [IssuerAPIModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ BE/issuer/src/issuer/issuer-api.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { IssuerAPIService } from './issuer-api.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { UserVCDto } from './user-vc.dto';
+
+@Controller('api/issuer')
+@ApiTags('Issuer API')
+export class IssuerAPIController {
+  constructor(private readonly issuerAPIService: IssuerAPIService) {}
+
+  @Get('/create-vc')
+  @ApiOperation({
+    summary: '사용자 VC 생성 후 블록체인에 키체인 적재',
+  })
+  async createUserVC(@Query() dto: UserVCDto): Promise<any> {
+    const vc = await this.issuerAPIService.createUserVC(dto);
+    const isLoadSuccess = await this.issuerAPIService.loadKeyChain(vc);
+    if (!isLoadSuccess) {
+      return 'Block Chain Load Failed';
+    }
+    const issuerPubKey = await this.issuerAPIService.getIssuerPubKey();
+    return { issuerPubKey, vc };
+  }
+}
