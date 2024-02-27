54ffcca2e9740d44624458df2159e687c2a3772d
edac99 <jamjam1208@naver.com>
2024-02-27 16:57:20 +0900

    test: 실패 테스트 코드 제거

--- BE/holder/test/app.test.spec.ts
+++ BE/holder/test/app.test.spec.ts
@@ -1,27 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { HolderAppModule } from '../src/app.module';
-import { IssuerAppModule } from './../../issuer/src/app.module';
 
 describe('AppController (e2e)', () => {
   let holderApp: INestApplication;
-  let issuerApp: INestApplication;
 
   beforeEach(async () => {
     const holderModuleFixture: TestingModule = await Test.createTestingModule({
       imports: [HolderAppModule],
     }).compile();
 
-    const issuerModuleFixture: TestingModule = await Test.createTestingModule({
-      imports: [IssuerAppModule],
-    }).compile();
-
     holderApp = holderModuleFixture.createNestApplication();
-    issuerApp = issuerModuleFixture.createNestApplication();
 
     await holderApp.init();
-    await issuerApp.init();
   });
 
   it('success test', () => {
@@ -30,6 +21,5 @@ describe('AppController (e2e)', () => {
 
   afterAll(async () => {
     await holderApp.close();
-    await issuerApp.close();
   });
 });
