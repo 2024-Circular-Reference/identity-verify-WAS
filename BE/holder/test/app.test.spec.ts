692e74a1dffa72dda1d1349bf457e7b6519de237
edac99 <jamjam1208@naver.com>
2024-02-27 17:04:07 +0900

    test: 외부 모듈 실행 시도

--- BE/holder/test/app.test.spec.ts
+++ BE/holder/test/app.test.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { HolderAppModule } from '../src/app.module';
 import { IssuerAppModule } from './../../issuer/src/app.module';
 
@@ -25,6 +26,20 @@ describe('AppController (e2e)', () => {
 
   it('success test', () => {
     return undefined;
+  });
+
+  it('create-vc to Issuer (Success)', () => {
+    const userVCDto = {
+      studentNumber: 'Test Student Number',
+      studentPassword: 'Test Student Password',
+      HolderPubKey: 'Test Holder Pub Key',
+    };
+
+    return request(holderApp.getHttpServer())
+      .get('/api/holder/create-vc')
+      .send(userVCDto)
+      .expect(200)
+      .expect('{"issuerPubKey":"Issuer Pub Key","vc":"vc"}');
   });
 
   afterAll(async () => {
