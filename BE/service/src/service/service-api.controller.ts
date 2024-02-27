1d18d555f0d0288a22113689c1d911dac77a0ab6
edac99 <jamjam1208@naver.com>
2024-02-27 18:05:05 +0900

    feat: serveNearCredit API 재생성
    필요에 의해 삭제했다가 재생성

--- BE/service/src/service/service-api.controller.ts
+++ BE/service/src/service/service-api.controller.ts
@@ -8,6 +8,14 @@ import { ProofDto } from './proof.dto';
 export class ServiceAPIController {
   constructor(private readonly serviceAPIService: ServiceAPIService) {}
 
+  @Get('near-credit')
+  @ApiOperation({
+    summary: '사용자 계정 생성시 Near Credit 요청에 대한 반환',
+  })
+  async serveNearCredit(): Promise<any> {
+    return await this.serviceAPIService.serveNearCredit();
+  }
+
   @Get('verify-proof')
   @ApiOperation({
     summary: '생성된 Proof를 검증하기 위해 Verifier 서버로 프록시',
